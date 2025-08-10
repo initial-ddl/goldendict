516046eef75222ba46833610bde01af3400deb42
initial-ddl <129729012+initial-ddl@users.noreply.github.com>
2025-08-10 20:48:25 +0800

    Update btreeidx.hh

--- src/dict/btreeidx.hh
+++ src/dict/btreeidx.hh
@@ -256,7 +256,7 @@ struct IndexedWords: public map< string, vector< WordArticleLink > >
   /// Instead of adding to the map directly, use this function. It does folding
   /// itself, and for phrases/sentences it adds additional entries beginning with
   /// each new word.
-  void addWord( const std::u32string & word, uint32_t articleOffset, unsigned int maxHeadwordSize = 100U );
+  void addWord( const std::u32string & word, uint32_t articleOffset, unsigned int maxHeadwordSize = 256U );
 
   /// Differs from addWord() in that it only adds a single entry. We use this
   /// for zip's file names.
